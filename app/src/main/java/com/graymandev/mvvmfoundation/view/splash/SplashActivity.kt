7a6d216c257548248677791f73b45fcea78f65e2
KevSalcedo <kevsalcedom@gmail.com>
2022-11-30 11:37:54 -0500

    background and status bar colors updated. Code updated to easy-binding.

--- app/src/main/java/com/graymandev/mvvmfoundation/view/splash/SplashActivity.kt
+++ app/src/main/java/com/graymandev/mvvmfoundation/view/splash/SplashActivity.kt
@@ -6,17 +6,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
 import android.view.animation.Animation
 import android.widget.TextView
+import com.chetantuteja.easybinding.BindingActivity
 import com.google.android.material.animation.AnimationUtils
 import com.graymandev.mvvmfoundation.R
 import com.graymandev.mvvmfoundation.databinding.ActivitySplashBinding
 import com.graymandev.mvvmfoundation.view.intro.IntroActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BindingActivity<ActivitySplashBinding>() {
 
-    private var binding : ActivitySplashBinding? = null
+    //private var binding : ActivitySplashBinding? = null
 
+    override fun setupViewBinding(inflater: LayoutInflater): ActivitySplashBinding{
+        return ActivitySplashBinding.inflate(inflater)
+    }
+
+    override fun init(){
+        val typeFace: Typeface = Typeface.createFromAsset(assets,"cookie regular.ttf")
+
+        binding?.tvAppName?.typeface = typeFace
+
+        // Here we will launch the Intro Screen after the splash screen using the handler.
+        // As using handler the splash screen will disappear after what we give to the handler.
+        // Adding the handler to after the a task after some delay.
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                // If the user is signed in once and not signed out again from the app. So next time while coming into the app
+                // we will redirect him to MainScreen or else to the Intro Screen as it was before.
+
+                // Get the current user id
+                //val currentUserID = FirestoreClass().getCurrentUserID()
+
+                /*
+                if (currentUserID.isNotEmpty()) {
+                    // Start the Main Activity
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                } else {
+                    // Start the Intro Activity
+                    startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+                }
+                 */
+
+                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+
+                // Call this when your activity is done and should be closed.
+                finish()
+            },
+            3500) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
+    }
+
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
@@ -53,8 +94,8 @@ class SplashActivity : AppCompatActivity() {
                 finish()
             },
             3500) // Here we pass the delay time in milliSeconds after which the splash activity will disappear.
-
-
     }
+
+     */
 
 }
